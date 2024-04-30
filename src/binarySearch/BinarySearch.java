5ac004e5e4137b74c36afe50c39ca7390c092fee
David Dutra <dadutra@hotmail.com>
2024-04-30 17:41:04 -0300

    Binary search done - Log2 N

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -1,4 +1,55 @@
 package binarySearch;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BinarySearch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= 100; i++ ) {
+            list.add(i);
+        }
+
+        System.out.print("Chouse a number: ");
+        int one = sc.nextInt();
+
+        System.out.println();
+
+        System.out.println("Try catch in..");
+
+        testLoop(list, one);
+
+        sc.close();
+    }
+
+    public static void testLoop(List<Integer> list, int one){
+        int low = 0;
+        int hight = list.size();
+
+        boolean flag = true;
+        int i = 0;
+
+        while (flag) {
+            int medium = (low + hight)/2;
+            i++;
+
+            if(one == medium) {
+                System.out.println("Catch!");
+                System.out.printf("Attempt: %d", i);
+                flag = false;
+            }
+
+            if (medium < one) {
+                low = medium + 1;
+            }
+
+            else if (medium > one) {
+                hight = medium - 1;
+            }
+        }
+    }
 }
