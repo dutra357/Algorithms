552b9afbabfe078b3ea102e44501aac72084db16
David Dutra <dadutra@hotmail.com>
2024-05-03 10:02:38 -0300

    Start square exeercise

--- src/quickSort/quickSort.java
+++ src/quickSort/quickSort.java
@@ -1,4 +1,33 @@
 package quickSort;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class quickSort {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Chose the first side of de solid: ");
+        int size1 = sc.nextInt();
+
+        System.out.println();
+
+        System.out.print("Now, chose the second: ");
+        int size2 = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Calculating.. ");
+
+
+        sc.close();
+    }
+
+
+
+    public static int findMaximunSquareDivision(int size1, int size2) {
+
+        return null;
+    }
 }
