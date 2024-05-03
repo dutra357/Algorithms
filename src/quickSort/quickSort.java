9f47416181dd7e57e0763784d0aa60b58e1f7774
David Dutra <dadutra@hotmail.com>
2024-05-03 13:10:06 -0300

    recursive problems

--- src/quickSort/quickSort.java
+++ src/quickSort/quickSort.java
@@ -9,6 +9,7 @@ public class quickSort {
         Scanner sc = new Scanner(System.in);
 
         //1680 x 640m
+        /*
         System.out.print("Chose the first side of de solid: ");
         Double size1 = sc.nextDouble();
 
@@ -21,44 +22,64 @@ public class quickSort {
         System.out.println("Calculating.. ");
         System.out.println();
 
-        List<Integer> numbers = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            Integer number = sc.nextInt();
-            numbers.add(number);
-        }
-
-        /*
         Double solve = findMaximunSquareDivision(size1, size2);
         System.out.printf("Minimum square area: %f", solve);
-         */
+        */
+
+        System.out.println("escolha numeros: ");
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite: ");
+           Integer number = sc.nextInt();
+           numbers.add(number);
+        }
+
+        System.out.println();
+        System.out.printf("Total: %d", count(numbers));
 
         sc.close();
     }
 
-    public static Integer calcNumbersRecursion(List<Integer> numbers) {
-        for (Integer x : numbers) {
-            x +
+    public static Integer count(List<Integer> numbers) {
+
+        Integer last = numbers.getLast();
+        numbers.remove(last);
+
+        if (!numbers.isEmpty()) {
+            count(numbers);
         }
-
-
+        return i;
     }
+
+    public static Integer calcNumbersRecursion(List<Integer> numbers) {
+        Integer sum = 0;
+        sum += numbers.getLast();
+
+        return null;
+        }
 
     public static Double findMaximunSquareDivision(Double size1, Double size2) {
         Double minor = size2;
+        Double hight = size1;
 
         if (size1 < size2) {
             minor = size1;
+            hight = size2;
         }
 
         int floor = (int) Math.floor(size1/minor);
-
+        if (floor < 2) {
+            return hight / minor;
+        }
         if (((floor * minor) - size1) % minor == 0) {
 
             return ((floor * minor) - size1) / minor;
         }
 
         double rec1 = ((floor * minor) - size1);
+
+
         if ((floor * minor) < size1) {
             rec1 = (size1 - (floor * minor));
         }
